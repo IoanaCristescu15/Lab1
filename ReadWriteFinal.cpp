21249eb047bb676fcd46bc98926e17e680b069ef
Mamnuya Rinki <69429491+mamnuya@users.noreply.github.com>
2021-08-29 17:15:49 -0400

    Improved error printing

--- ReadWriteFinal.cpp
+++ ReadWriteFinal.cpp
@@ -14,12 +14,12 @@ To compile this program:  g++ example.cpp -o example
  */
 
 /*
-Documentation: 
+Documentation:
 - For reading the input file line by line, in order to tokenize the input, we tried using the
 strtok function (no exterior sourse as Ioana learned it in high school). However, we discovered that this function
 would only work with character arrays that contain strings. Then we learned about the standard stream istringstream
 from the following sourse: https://stackoverflow.com/questions/57409206/how-to-fix-no-matching-function-for-call-to-strtok".
-- stoi fuunction: https://www.cplusplus.com/reference/string/stoi/ 
+- stoi fuunction: https://www.cplusplus.com/reference/string/stoi/
 */
 
 
@@ -31,7 +31,7 @@ from the following sourse: https://stackoverflow.com/questions/57409206/how-to-f
 using namespace std;
 
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     // If there are more or less arguments than the names of the input and output files
     if (argc != 3)
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 
     if (!fin.is_open())
     {
-        // if file can't be opened, need to let the user know
+        // Error printing if file can't be opened
         cerr << "Error: could not open file: " << argv[1] << "\n";
         exit(1);
     }
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
     int readStudentId, readYearOfStudy;
     string line;
 
-    // Reading the input file and making sure it has the correct format; print an error otherwise 
+    // Reading the input file and making sure it has the correct format; print an error otherwise
     while(getline(fin, line))
     {
         if (line == "")
@@ -75,50 +75,64 @@ int main(int argc, char *argv[])
             numberWords += 1;
             if (numberWords == 1)
             {
-                firstName.push_back(word);
+              //Error printing in case a first name contains any numerical characters
+              if (word.find_first_of("0123456789") != string::npos){
+                  cerr << "Error: bad file format regarding numbers existing in the [(string) FirstName] field in file: " << argv[1] << "\n";
+                  exit(1);
+              }
+              firstName.push_back(word);
             }
             else
                 if (numberWords == 2)
                 {
+                  //Error printing in case a last name contains any numerical characters
+                  if (word.find_first_of("0123456789") != string::npos){
+                      cerr << "Error: bad file format regarding numbers existing in the [(string) LastName] field in file: " << argv[1] << "\n";
+                      exit(1);
+                  }
                     lastName.push_back(word);
                 }
                 else
                     if (numberWords == 3)
                     {
-                        try 
+                        try
                         {
                             string::size_type sz;
                             int integer = stoi(word, &sz);
+                            //Error printing in case a student id contains atleast one alphabetical letter and number
                             if (word.substr(sz) != "")
                             {
-                                cerr << "Error: bad file format: " << argv[1] << "\n";
+                                cerr << "Error: bad file format regarding [(int) student ID] field in file: " << argv[1] << ". Reminder: [(int) student ID] must be an integer, and not include alphabetical letters."<< "\n";
                                 exit(1);
                             }
                             studentId.push_back(integer);
                         }
+                        //Error printing in case a student id contains only alphabetical letters and no numbers
                         catch(exception &err)
                         {
-                            cerr << "Error: bad file format: " << argv[1] << "\n";
+                            cerr << "Error: bad file format: " << argv[1] << " causing the following exception: " << err.what() << ". Reminder: An 'stoi: no conversion' exception may be caused by alphabetical letters in a [(int) student ID] field."<< "\n";
                             exit(1);
                         }
                     }
                     else
                         if (numberWords == 4)
                         {
-                            try 
+                            try
                             {
                                 string::size_type sz;
                                 int integer = stoi(word, &sz);
+                                //Error printing in case a year of study contains atleast one alphabetical letter and number
                                 if (word.substr(sz) != "")
                                 {
-                                    cerr << "Error: bad file format: " << argv[1] << "\n";
+                                    cerr << "Error: bad file format regarding [(int) year of study] field in file: " << argv[1] << ". Reminder: [(int) year of study] must be an integer, and not include alphabetical letters."<< "\n";
                                     exit(1);
                                 }
                                 yearOfStudy.push_back(integer);
                             }
+                            //Error printing in case a year of study contains only alphabetical letters and no numbers
                             catch(exception &err)
                             {
-                                cerr << "Error: bad file format: " << argv[1] << "\n";
+                                cerr << "Error: bad file format: " << argv[1] << " causing the following exception: " << err.what() << ". Reminder: An 'stoi: no conversion' exception may be caused by alphabetical letters in a [(int) year of study] field."<< "\n";
                                 exit(1);
                             }
                         }
@@ -128,10 +142,12 @@ int main(int argc, char *argv[])
                             exit(1);
                         }
         }
+
+        //Error printing in case the input file does not have exactly 4 fields per line
         if (numberWords != 4)
         {
-            cerr << "Error: bad file format: " << argv[1] << "\n";
-            exit(1);
+          cerr << "Error: incorrect field numbers regarding required 4 fields of [(string) FirstName] [(string) LastName] [(int) student ID] [(int) year of study] in file: " << argv[1] << "\n";
+          exit(1);
         }
     }
 
